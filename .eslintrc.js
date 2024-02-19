ca7dc0c63014b54227cc20b26636ba6c259843cb
kevi0x6e <kevinnk16@gmail.com>
2024-02-18 22:11:49 -0300

    ci(test-pipeline): add code review

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "jest": true
+    },
+    "rules": {
+        "no-console": "warn",
+        "space-before-blocks": "error",
+        "keyword-spacing": "error",
+    },
+  };
+  
