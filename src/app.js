aab5686eb8d286179176a208420f6105acae793a
kevi0x6e <kevinnk16@gmail.com>
2024-02-25 22:32:25 -0300

    chore: update express version

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   const {printHelloToYou} = require('./services/printHello');
